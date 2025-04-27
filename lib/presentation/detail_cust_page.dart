0e9f89d29b069bc9c61b0bc929cdd8ef49d60536
LYChubby <f4uzi345@gmail.com>
2025-04-28 01:33:59 +0700

    Membuat Halaman Detail Cust ( Gambar dan Textform)

--- lib/presentation/detail_cust_page.dart
+++ lib/presentation/detail_cust_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:ucp1/presentation/home_page.dart';
 
 class DetailCustPage extends StatelessWidget {
+  final String namaPengguna;
   final String nama;
   final String alamat;
   final String noHp;
@@ -9,6 +11,7 @@ class DetailCustPage extends StatelessWidget {
   final String kodePos;
   const DetailCustPage({
     super.key,
+    required this.namaPengguna,
     required this.nama,
     required this.alamat,
     required this.noHp,
@@ -19,6 +22,138 @@ class DetailCustPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange,
+        elevation: 0,
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.arrow_back, color: Colors.white),
+          ),
+        ),
+        title: Text(
+          "Detail $nama",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(16.0),
+        children: [
+          Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ClipOval(
+                  child: Image.asset(
+                    "assets/fri.jpeg",
+                    width: 150, // Lebar dan tinggi harus sama
+                    height: 150,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  nama,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10),
+                Text(email, style: TextStyle(fontSize: 16)),
+                const SizedBox(height: 10),
+                Text(noHp, style: TextStyle(fontSize: 16)),
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Alamat"),
+              const SizedBox(height: 10),
+              TextFormField(
+                initialValue: alamat,
+                readOnly: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Provinsi"),
+                        const SizedBox(height: 10),
+                        TextFormField(
+                          initialValue: provinsi,
+                          readOnly: true,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 15),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Kode Pos"),
+                        const SizedBox(height: 10),
+                        TextFormField(
+                          initialValue: kodePos,
+                          readOnly: true,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 40),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(
+                        builder:
+                            (context) => HomePage(namaPengguna: namaPengguna),
+                      ),
+                      (route) => false,
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.deepOrange,
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  child: Text("Selesai"),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
