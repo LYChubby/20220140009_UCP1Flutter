7fa8f69b1345801d6d226b4bc319363b4e047b95
LYChubby <f4uzi345@gmail.com>
2025-04-27 23:34:47 +0700

    Membuat Hakaman Detail Piket

---
+++ lib/presentation/detail_piket_page.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class DetailPiketPage extends StatelessWidget {
+  final String tugas;
+  final String tanggal;
+  final String namaPengguna;
+  const DetailPiketPage({
+    super.key,
+    required this.tugas,
+    required this.namaPengguna,
+    required this.tanggal,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final formattedDate = DateFormat(
+      'EEEE, dd MMMM yyyy',
+      'id_ID',
+    ).format(DateTime.parse(tanggal));
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange,
+        elevation: 0,
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.arrow_back, color: Colors.white),
+          ),
+        ),
+        title: Text(
+          "Detail $tugas",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Text(
+                    formattedDate,
+                    style: TextStyle(
+                      color: Colors.deepOrange,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 10),
+                  padding: const EdgeInsets.all(12.0),
+                  decoration: BoxDecoration(
+                    color: Colors.deepOrange,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text(
+                    namaPengguna,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+            Container(
+              width: double.infinity,
+              margin: const EdgeInsets.only(bottom: 10),
+              padding: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.deepOrange,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Text(tugas, style: TextStyle(color: Colors.white)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
