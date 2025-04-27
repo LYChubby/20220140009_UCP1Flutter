f4e7fb3adad9c900292a3096c64c71049cdb8447
LYChubby <f4uzi345@gmail.com>
2025-04-27 19:47:24 +0700

    Membuat Navigasi Data Piket, Pelanggan dan Barang

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:ucp1/presentation/data_barang_page.dart';
+import 'package:ucp1/presentation/data_pelanggan_page.dart';
+import 'package:ucp1/presentation/data_piket_page.dart';
 
 class HomePage extends StatelessWidget {
   final String namaPengguna;
@@ -50,89 +53,123 @@ class HomePage extends StatelessWidget {
         padding: EdgeInsets.all(16.0),
         child: Column(
           children: [
-            Image.network(
-              "https://img.freepik.com/premium-vector/hand-drawn-pempek-palembang-indonesian-traditional-food-background_141928-606.jpg",
-              fit: BoxFit.cover,
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.network(
+                "https://img.freepik.com/premium-vector/hand-drawn-pempek-palembang-indonesian-traditional-food-background_141928-606.jpg",
+                fit: BoxFit.cover,
+              ),
             ),
             const SizedBox(height: 50),
             Row(
               children: [
                 Expanded(
-                  child: Container(
-                    padding: const EdgeInsets.only(
-                      left: 10,
-                      right: 10,
-                      top: 50,
-                      bottom: 50,
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.deepOrange,
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.group_add, size: 60, color: Colors.white),
-                        Text(
-                          "Data Piket",
-                          style: TextStyle(color: Colors.white),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DataPiketPage(),
                         ),
-                      ],
+                      );
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.only(
+                        left: 10,
+                        right: 10,
+                        top: 50,
+                        bottom: 50,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.deepOrange,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.group_add, size: 60, color: Colors.white),
+                          const SizedBox(height: 8),
+                          Text(
+                            "Data Piket",
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
                 const SizedBox(width: 16),
                 Expanded(
-                  child: Container(
-                    padding: const EdgeInsets.only(
-                      left: 10,
-                      right: 10,
-                      top: 50,
-                      bottom: 50,
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.deepOrange,
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.dashboard_customize_rounded,
-                          size: 60,
-                          color: Colors.white,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DataPelangganPage(),
                         ),
-                        Text(
-                          "Data Pelanggan",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
+                      );
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.only(
+                        left: 10,
+                        right: 10,
+                        top: 50,
+                        bottom: 50,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.deepOrange,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.dashboard_customize_rounded,
+                            size: 60,
+                            color: Colors.white,
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            "Data Pelanggan",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ],
             ),
             const SizedBox(height: 20),
-            Container(
-              padding: const EdgeInsets.all(50),
-              width: 400,
-              decoration: BoxDecoration(
-                color: Colors.deepOrange,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.receipt_long_rounded,
-                    size: 60,
-                    color: Colors.white,
-                  ),
-                  Text(
-                    "Barang Masuk / Keluar",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ],
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => DataBarangPage()),
+                );
+              },
+              child: Container(
+                padding: const EdgeInsets.all(50),
+                width: 400,
+                decoration: BoxDecoration(
+                  color: Colors.deepOrange,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.receipt_long_rounded,
+                      size: 60,
+                      color: Colors.white,
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      "Barang Masuk / Keluar",
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
