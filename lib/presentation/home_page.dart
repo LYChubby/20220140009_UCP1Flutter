6ee0f97e69ba245629209ee6ddfde39a5219c9ab
LYChubby <f4uzi345@gmail.com>
2025-04-27 21:00:27 +0700

    Pharsing Data Dari Home Ke Data Piket

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -69,7 +69,9 @@ class HomePage extends StatelessWidget {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) => DataPiketPage(),
+                          builder:
+                              (context) =>
+                                  DataPiketPage(namaPengguna: namaPengguna),
                         ),
                       );
                     },
