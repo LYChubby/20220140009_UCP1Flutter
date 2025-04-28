4ddb54f45f0bd92464426441af1594e28e3b0bfe
LYChubby <f4uzi345@gmail.com>
2025-04-28 22:58:53 +0700

    Membuat Halaman Detail Barang

---
+++ lib/presentation/detail_barang_page.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:ucp1/presentation/home_page.dart';
+
+class DetailBarangPage extends StatelessWidget {
+  final String namaPengguna;
+  final String tanggal;
+  final String jenisTransaksi;
+  final String jenisBarang;
+  final String jumlahBarang;
+  final String hargaSatuan;
+  final String totalHarga;
+  const DetailBarangPage({
+    super.key,
+    required this.namaPengguna,
+    required this.tanggal,
+    required this.jenisTransaksi,
+    required this.jenisBarang,
+    required this.jumlahBarang,
+    required this.hargaSatuan,
+    required this.totalHarga,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        padding: const EdgeInsets.only(
+          top: 70.0,
+          left: 16.0,
+          right: 16.0,
+          bottom: 16.0,
+        ),
+        children: [
+          Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ClipOval(
+                  child: Image.asset(
+                    "assets/fri.jpeg",
+                    width: 150, // Lebar dan tinggi harus sama
+                    height: 150,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Text(
+                  "Data Berhasil Disimpan",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Tanggal", style: TextStyle(fontSize: 20)),
+                    Text(tanggal, style: TextStyle(fontSize: 20)),
+                  ],
+                ),
+                const Divider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Jenis Transaksi", style: TextStyle(fontSize: 20)),
+                    Text(jenisTransaksi, style: TextStyle(fontSize: 20)),
+                  ],
+                ),
+                const Divider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Jenis Barang", style: TextStyle(fontSize: 20)),
+                    Text(jenisBarang, style: TextStyle(fontSize: 20)),
+                  ],
+                ),
+                const Divider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Jumlah Barang", style: TextStyle(fontSize: 20)),
+                    Text(jumlahBarang, style: TextStyle(fontSize: 20)),
+                  ],
+                ),
+                const Divider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Harga Satuan", style: TextStyle(fontSize: 20)),
+                    Text(hargaSatuan, style: TextStyle(fontSize: 20)),
+                  ],
+                ),
+                const Divider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Total Harga", style: TextStyle(fontSize: 20)),
+                    Text(
+                      NumberFormat.currency(
+                        locale: 'id_ID',
+                        symbol: "Rp. ",
+                        decimalDigits: 2,
+                      ).format(int.parse(totalHarga)),
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
+                const Divider(),
+                const SizedBox(height: 30),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder:
+                              (context) => HomePage(namaPengguna: namaPengguna),
+                        ),
+                        (route) => false,
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.deepOrange,
+                      foregroundColor: Colors.white,
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    child: Text("Selesai"),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
