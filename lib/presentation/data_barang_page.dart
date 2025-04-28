2279e2ef41c5c3a7cdb0a84991c855093e790abd
LYChubby <f4uzi345@gmail.com>
2025-04-28 12:43:19 +0700

    Membuat File Untuk Data Barang

---
+++ lib/presentation/data_barang_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DataBarangPage extends StatefulWidget {
+  const DataBarangPage({super.key});
+
+  @override
+  State<DataBarangPage> createState() => _DataBarangPageState();
+}
+
+class _DataBarangPageState extends State<DataBarangPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
