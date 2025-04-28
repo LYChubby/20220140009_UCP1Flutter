7d994c1af46bbe75f1c2b4cb6d9487fe94a79be1
LYChubby <f4uzi345@gmail.com>
2025-04-28 21:59:39 +0700

    Mmebuat Form Untuk Data Barang dan Menghapus Controller Jenis Barang dan Transaksi

--- lib/presentation/data_barang_page.dart
+++ lib/presentation/data_barang_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 class DataBarangPage extends StatefulWidget {
   const DataBarangPage({super.key});
@@ -10,9 +13,41 @@ class DataBarangPage extends StatefulWidget {
 class _DataBarangPageState extends State<DataBarangPage> {
   final TextEditingController tanggalController = TextEditingController();
   final TextEditingController jumlahBarangController = TextEditingController();
-  final TextEditingController jenisTransController = TextEditingController();
-  final TextEditingController jenisBarangController = TextEditingController();
   final TextEditingController hargaController = TextEditingController();
+  String? dateError;
+  DateTime? selectedDate;
+  String? selectedValueTransaksi;
+  List<String> jenisTransaksi = ['Barang Masuk', 'Barang Keluar'];
+  String? selectedValueBarang;
+  List<String> jenisBarang = ['Carrier', 'Sleeping Bag', 'Tenda', 'Sepatu'];
+
+  Future<void> _selectDate() async {
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2025),
+      lastDate: DateTime(2030),
+    );
+
+    if (pickedDate != null) {
+      setState(() {
+        selectedDate = pickedDate;
+        tanggalController.text =
+            tanggalController.text = DateFormat(
+              'EEEE, dd MMMM yyyy',
+              'id_ID',
+            ).format(selectedDate!);
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initializeDateFormatting('id_ID', null).then((_) {
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,6 +70,156 @@ class _DataBarangPageState extends State<DataBarangPage> {
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Tanggal Transaksi"),
+              const SizedBox(height: 10),
+              TextFormField(
+                controller: tanggalController,
+                readOnly: true,
+                decoration: InputDecoration(
+                  hintText: "Tanggal Transaksi",
+                  prefixIcon: IconButton(
+                    onPressed: _selectDate,
+                    icon: Icon(Icons.date_range),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Tanggal tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              DropdownButtonFormField(
+                decoration: InputDecoration(
+                  labelText: "Jenis Transaksi",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                value: selectedValueTransaksi,
+                items:
+                    jenisTransaksi
+                        .map(
+                          (String item) =>
+                              DropdownMenuItem(value: item, child: Text(item)),
+                        )
+                        .toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    selectedValueTransaksi = newValue;
+                  });
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Pilih Jenis Transaksi';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              DropdownButtonFormField(
+                decoration: InputDecoration(
+                  labelText: "Jenis Barang",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                value: selectedValueBarang,
+                items:
+                    jenisBarang
+                        .map(
+                          (String item) =>
+                              DropdownMenuItem(value: item, child: Text(item)),
+                        )
+                        .toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    selectedValueBarang = newValue;
+                  });
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Pilih Jenis Barang';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Jumlah Barang"),
+                        const SizedBox(height: 10),
+                        TextFormField(
+                          controller: jumlahBarangController,
+                          keyboardType: TextInputType.number,
+                          inputFormatters: [
+                            FilteringTextInputFormatter
+                                .digitsOnly, // Hanya menerima digit
+                          ],
+                          decoration: InputDecoration(
+                            hintText: "Jumlah Barang",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Jumlah Barang tidak boleh kosong';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 15),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Harga Satuan"),
+                        const SizedBox(height: 10),
+                        TextFormField(
+                          controller: hargaController,
+                          readOnly: true,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.currency_bitcoin),
+                            hintText: "Harga Satuan",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Harga tidak boleh kosong';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
